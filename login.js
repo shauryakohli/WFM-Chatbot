ca0d276602ce07f04902cd9b456ff92d73bc2a18
Shaurya Kohli <shauryakohli17@gmail.com>
2025-02-09 19:44:19 +0530

    Added login.js

---
+++ login.js
@@ -0,0 +1,5 @@
+document.getElementById('login-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Perform login validation here if needed
+    window.location.href = 'index.html';
+});
